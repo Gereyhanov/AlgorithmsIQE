2966ea69f53b610f48bb5559eeedcd6e65ab6f04
Rizvan Gereykhanov <gereyhanov@gmail.com>
2016-02-01 09:46:42 +0300

    Create base structure, add AreaChart

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,18 +4,48 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.SceneBuilder;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.stage.Stage;
+import sample.inheritance.CurvedFittedAreaChart;
+
+import java.io.IOException;
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+    @Override public void start(Stage primaryStage) throws IOException {
+
+        Parent root = FXMLLoader.load(getClass().getResource("view/sample.fxml"));
+        primaryStage.setTitle("JavaFX Chart (Series)");
+
+        // create chart and set as center content
+        CurvedFittedAreaChart chart = new CurvedFittedAreaChart(
+                new NumberAxis(), new NumberAxis());
+        chart.setLegendVisible(true);
+        chart.setHorizontalGridLinesVisible(false);
+        chart.setVerticalGridLinesVisible(false);
+        chart.setAlternativeColumnFillVisible(false);
+        chart.setAlternativeRowFillVisible(false);
+
+        final XYChart.Series<Number, Number> series = new XYChart.Series<Number, Number>();
+        series.setName("sin(x)");
+
+        for(int i=0; i<30; i++){
+            series.getData().addAll(
+                    new XYChart.Data<Number,Number>(i, Math.sin(i))
+            );
+        }
+        chart.getData().add(series);
+
+        primaryStage.setScene(SceneBuilder.create()
+                .root(chart)
+                .width(1500).height(800)
+                // .stylesheets(CurveFittedChart.class.getResource("CurveFittedChart.css").toExternalForm())
+                .build()
+        );
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
